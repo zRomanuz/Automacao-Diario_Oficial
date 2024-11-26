7d3469486d17bf967b0ddccdb087864364f55106
Guilherme-AFranco <guilherme.ap.franco@usp.br>
2024-11-26 00:54:13 -0300

    files update

---
+++ automation_diario_ofc.py
@@ -0,0 +1,84 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service 
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC 
+import time
+import pyautogui 
+
+service = Service(executable_path="E:\Backup_PC\Aplicativos\ChromeDrive\WebDriver\chromedriver.exe")
+driver = webdriver.Chrome(service=service)
+driver.maximize_window()
+
+driver.get("https://in.gov.br/leiturajornal")
+
+time.sleep(2)
+
+WebDriverWait(driver, 2).until(
+    EC.presence_of_all_elements_located((By.ID, "toggle-search-advanced"))
+)
+
+advance_search = driver.find_element(By.ID, "toggle-search-advanced")
+advance_search.click()
+ 
+
+time.sleep (1)
+WebDriverWait(driver, 12).until(
+    EC.presence_of_all_elements_located((By.ID, "do3"))
+)
+
+secao3 = driver.find_element(By.ID, "do3")
+secao3.click()
+
+time.sleep(2)
+
+dia = driver.find_element(By.ID, "mes") # Voltar para "dia" dps (tinha poucas noticias com parametro "dia")
+dia.click()
+
+WebDriverWait(driver, 2).until(
+    EC.presence_of_all_elements_located((By.CLASS_NAME, "form-control"))
+)
+
+
+search_area = driver.find_element(By.CLASS_NAME, "form-control")
+search_area.clear()
+search_area.send_keys("aircraft" + Keys.ENTER)
+time.sleep(10)
+
+
+## Obtendo as noticias ##
+noticias = driver.find_elements(By.CLASS_NAME, "resultados-wrapper") # Obtem as classes de noticias existentes na aba
+titulo_dou = {} # Dicionario para colocar o titulo da notícia
+texto_dou = {} # Dicionario para o Gemini trabalhar
+for i, noticia in enumerate(noticias):
+    try:
+        link_element = noticia.find_element(By.TAG_NAME, "a") # Encontrar link
+        noticia_url = link_element.get_attribute("href") # Coletar o link
+
+        driver.execute_script("window.open(arguments[0]);", noticia_url) # Abrir em nova aba
+        driver.switch_to.window(driver.window_handles[1]) # Ir para a nova aba
+
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_all_elements_located((By.CLASS_NAME, "dou-paragraph"))
+        ) # Esperar o texto da notícia carregar
+
+        titulo_dou[f'Noticia {i}'] = driver.find_element(By.CLASS_NAME, "identifica").text # Coletar titulo
+        texto_dou[f'Noticia {i}'] = driver.find_element(By.CLASS_NAME, "texto-dou").text # Coletar notícia
+
+        print(f'\nTítulo da Notícia {i+1}:')
+        print(titulo_dou[f'Noticia {i}'])
+        print(f'Texto da Notícia {i+1}:')
+        print(texto_dou[f'Noticia {i}'])
+
+        driver.close() # Fechar a aba adicional
+        driver.switch_to.window(driver.window_handles[0])  # Voltar à aba principal
+
+        noticias = driver.find_elements(By.CLASS_NAME, "resultados-wrapper") # Atualizar a lista das notícias na pagina
+    except Exception as e:
+        print(f"Erro ao processar a notícia {i+1}: {e}") # Se der erro, ele avisa e ficamos tristes
+
+driver.quit()
+
+   
